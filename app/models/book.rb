fafe98b1fac4d2afae72d6364bec0a6eeb0a95c8
saitohanta <osyogatu17@icloud.com>
2023-05-07 07:36:17 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
@@ -22,5 +22,5 @@ class Book < ApplicationRecord
       @book = Book.all
     end
   end
-  
+
 end
